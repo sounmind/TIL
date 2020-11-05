6c9a729e6691aaf99f421012e4c4cab2d5a534bf
sounmind <sounmind777@gmail.com>
2020-11-05 19:49:35 +0900

    구현 문제 3 | 교재 풀이 방식 참고 코드

--- this-is-coding-test-practice/implementation-3.py
+++ this-is-coding-test-practice/implementation-3.py
@@ -1,30 +1,30 @@
 import time
 
-row = "0abcdefgh"
 loc = input()
 start_time = time.time()
 chances = []
-x = int(row.index(loc[0]))  # 방법1: 리스트 인덱스 활용하여 얻기
 x = int(ord(loc[0])) - int(ord("a")) + 1  # 방법2: 아스키코드로 int형 좌표 얻기
 y = int(loc[1])
 
 
 # 하나의 위치에 따라 8개의 이동 가능한 위치가 있다.
 chances = [
-    [x - 2, y - 1],
-    [x - 2, y + 1],
-    [x - 1, y + 2],
-    [x - 1, y - 2],
-    [x + 1, y - 2],
-    [x + 1, y + 2],
-    [x + 2, y - 1],
-    [x + 2, y + 1],
+    [-2, -1],
+    [-2, +1],
+    [-1, +2],
+    [+1, -2],
+    [-1, -2],
+    [+1, +2],
+    [+2, -1],
+    [+2, +1],
 ]
 
 # 해당 위치가 체스판을 벗어나는지 확인하고 가능한 경우를 계산한다
 count = 8
 for chance in chances:
-    if chance[0] < 1 or chance[1] < 1:
+    next_x = x + chance[0]
+    next_y = y + chance[1]
+    if (8 < next_x or next_x < 1) or (8 < next_y or next_y < 1):
         count -= 1
 
 print(count)
